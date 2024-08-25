0412d31bed9cd4f5ad63c7889c93843a350d1f01
Lucas Alcântara <alcantaralucas2002@gmail.com>
2024-08-25 18:51:11 -0300

    Create 3.1- Exercícios.py

---
+++ 3.1- Exercícios.py
@@ -0,0 +1,85 @@
+# Exercícios de Operadores
+
+# Dinheiro
+maria = 50
+joão = 60
+pedro = 120
+marta = 75
+
+# Variáveis de Diferença
+diferença_maria_joão = maria - joão
+diferença_maria_pedro = maria - pedro
+diferença_maria_marta = maria - marta
+diferença_joão_maria = joão - maria
+diferença_joão_pedro = joão - pedro
+diferença_joão_marta = joão - marta
+diferença_pedro_maria = pedro - maria
+diferença_pedro_joão = pedro - joão
+diferença_pedro_marta = pedro - marta
+diferença_marta_maria = marta - maria
+diferença_marta_joão = marta - joão
+diferença_marta_pedro = marta - pedro
+
+# Condicionais Maria
+if diferença_maria_joão > diferença_joão_maria:
+    print("Maria tem mais Dinheiro do que João")
+else:
+    print("Maria tem menos Dinheiro do que João!")
+
+if diferença_maria_pedro > diferença_pedro_maria:
+    print("Maria tem mais Dinheiro do que Pedro")
+else:
+    print("Maria tem menos Dinheiro do que Pedro")
+
+if diferença_maria_marta > diferença_marta_maria:
+    print("Maria tem mais Dinheiro do que Marta")
+else:
+    print("Maria tem menos Dinheiro do que Marta")
+
+# Condicionais João
+if diferença_joão_maria > diferença_maria_joão:
+    print("João tem mais Dinheiro do que Maria")
+else:
+    print("João tem menos Dinheiro do que Maria!")
+
+if diferença_joão_pedro > diferença_pedro_joão:
+    print("João tem mais Dinheiro do que Pedro")
+else:
+    print("João tem menos Dinheiro do que Pedro")
+
+if diferença_joão_marta > diferença_marta_joão:
+    print("João tem mais Dinheiro do que Marta")
+else:
+    print("João tem menos Dinheiro do que Marta")
+
+# Condicionais Pedro
+if diferença_pedro_maria > diferença_maria_pedro:
+    print("Pedro tem mais Dinheiro do que Maria")
+else:
+    print("Pedro tem menos Dinheiro do que Maria!")
+
+if diferença_pedro_joão > diferença_joão_pedro:
+    print("Pedro tem mais Dinheiro do que João")
+else:
+    print("Pedro tem menos Dinheiro do que João")
+
+if diferença_pedro_marta > diferença_marta_pedro:
+    print("Pedro tem mais Dinheiro do que Marta")
+else:
+    print("Pedro tem menos Dinheiro do que Marta")
+
+# Condicionais Marta
+if diferença_marta_maria > diferença_maria_marta:
+    print("Marta tem mais Dinheiro do que Maria")
+else:
+    print("Marta tem menos Dinheiro do que Maria!")
+
+if diferença_marta_joão > diferença_joão_marta:
+    print("Marta tem mais Dinheiro do que João")
+else:
+    print("Marta tem menos Dinheiro do que João")
+
+if diferença_marta_pedro > diferença_pedro_marta:
+    print("Marta tem mais Dinheiro do que Pedro")
+else:
+    print("Marta tem menos Dinheiro do que Pedro")
