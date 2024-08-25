7259c02138823439880141cd2978593b302f0160
Lucas Alcântara <alcantaralucas2002@gmail.com>
2024-08-25 11:01:29 -0300

    Create 1.1 Exercícios.py

---
+++ 1.1 Exercícios.py
@@ -0,0 +1,12 @@
+# "Exercício Básico"
+
+básico = "Olá Mundo!"
+básico1 = ["Olá Mundo!", "Adeus Mundo!"]
+
+print(básico) # Outra forma de chamar (usando variável)
+print("Olá Mundo!")
+
+for básico1 in básico1:
+    print("Eu não sei se digo:", básico1)
+
+# ficou depressivo isso kkkkk
